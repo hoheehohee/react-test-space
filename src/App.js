0ff80fd11a68db6c6595048e28507e15ab8c44b7
hohee <boycat87@gmail.com>
2018-09-11 11:14:13 +0900

    front setting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import { 
   Main,
   FormsyReact,
   MaterialUI,
   ReactInfinite } from './containers';
-
 class App extends Component {
 
   render() {
     return (
-      <Router className="App">
+      <Router>
         <div>
           <Route exact path='/' component={Main} />
           <Route path='/FormsyReact' component={FormsyReact} />
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,14 +1,83 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  List,
+  ListItem,
+  Grid,
+  Paper,
+  withStyles
+} from '@material-ui/core'
+
+const styles = theme => ({
+  root: {
+  flexGrow: 1
+  },
+  paper: {
+    padding: 0,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  list: {
+    padding: 0
+  },
+  item: {
+    justifyContent: 'center',
+    paddingTop: theme.spacing.unit * 4,
+    paddingBottom: theme.spacing.unit * 5
+  }
+});
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
   render() {
+    const { classes } = this.props;
     return (
-      <div>Main</div>
+      <div className={classes.root}>
+        <Grid container spacing={16}>
+          <Grid item xs={12}>
+            <Link to='/FormsyReact'>
+              <Paper className={classes.paper}>
+                <List className={classes.list}>
+                  <ListItem className={classes.item} button >
+                    FormsyReact
+                  </ListItem>
+                </List>
+              </Paper>
+            </Link>
+            <Link to='/MaterialUI'>
+              <Paper className={classes.paper}>
+                <List className={classes.list}>
+                  <ListItem className={classes.item} button>
+                    MaterialUI
+                  </ListItem>
+                </List>
+              </Paper>
+            </Link>
+            <Link to='/ReactInfinite'>
+              <Paper className={classes.paper}>
+                <List className={classes.list}>
+                  <ListItem className={classes.item} button>
+                    ReactInfinite
+                  </ListItem>
+                </List>
+              </Paper>
+            </Link>
+            <Link to='/JSON'>
+              <Paper className={classes.paper}>
+                <List className={classes.list}>
+                  <ListItem className={classes.item} button>
+                    JSON
+                  </ListItem>
+                </List>
+              </Paper>
+            </Link>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default Main;
+export default withStyles(styles)(Main);
